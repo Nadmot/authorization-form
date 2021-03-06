9535bfd971a1cb8a1c18939c3fdf597a782c7f88
Nadmot <80123213+Nadmot@users.noreply.github.com>
2021-03-07 01:48:15 +0300

    move css and js into separate files

---
+++ main.js
@@ -0,0 +1,6 @@
+function printFormData() {
+  const login = document.getElementById("login").value;
+  const password = document.getElementById("password").value;
+  console.log(login);
+  console.log(password);
+}
